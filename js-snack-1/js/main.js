e270241a22ba7feaa87e40b2ea5097ec9b340136
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-11-08 18:30:18 +0100

    function player data printing and crate array

--- js-snack-1/js/main.js
+++ js-snack-1/js/main.js
@@ -16,23 +16,22 @@
 
 // create player object
 const player = {
-    player_code : '',
+    player_code : player_code_generation(),
     name : 'Player_name',
     surname : 'Player_surname',
     age : 30,
-    game_points_average : '',
-    percentage_three_points : 5,
+    game_points_average : game_average(),
+    percentage_three_points : percentage_three_points_generation(),
 }
 
 // FUNCTIONS
-// player code generation
-function player_code_geration() {
+// player code generation - punto 1
+function player_code_generation() {
 
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
           numbers = '0123456789';
     let letter_part = '',
-    numbers_part = '',
-    code = '';
+        numbers_part = '';
 
     // generation of 3 letters
     for ( let i = 0; i < 3; i++ ) {
@@ -42,29 +41,137 @@ function player_code_geration() {
         numbers_part += numbers.charAt(Math.floor(Math.random() * numbers.length));
 
         // concatenazione delle variabili precedenti per la creazione del codice
-        code = letter_part + numbers_part;
+        player_code = letter_part + numbers_part;
     }
-    return code;
+    return player_code;
 }
-console.log( player_code_geration() );
-
-// game point average generation
+// game point average generation - punto 2
 function game_average() {
     const average = Math.floor( Math.random(0) * 50 );
-    return average
+    return average;
 }
-console.log( game_average() );
-// percentage three points generation
+// percentage three points generation - punto 3
 function percentage_three_points_generation() {
     const random_number = Math.floor( Math.random(0) * 100 );
     const percentage = (100 * random_number) / 100;
     return String( percentage + '%' );
 }
+// player data printing
+function player_data_printing() {
+    // destrutturazione dell'oggetto 'player ed estrazione dati'
+    const {player_code, name, surname, age} = player;
+    return {player_code, name, surname, age};
+}
+
+
+// creazione dell'array di 10 giocatori
+const players = [
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_1',
+        surname : 'Player_surname_1',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_2',
+        surname : 'Player_surname_2',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_3',
+        surname : 'Player_surname_3',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_4',
+        surname : 'Player_surname_4',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_5',
+        surname : 'Player_surname_5',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_6',
+        surname : 'Player_surname_6',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_7',
+        surname : 'Player_surname_7',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_8',
+        surname : 'Player_surname_8',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_9',
+        surname : 'Player_surname_9',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+    {
+        player_code : player_code_generation(),
+        name : 'Player_name_10',
+        surname : 'Player_surname_10',
+        age : 30,
+        game_points_average : game_average(),
+        percentage_three_points : percentage_three_points_generation(),
+    },
+]
+
+
+
+const player1 = {
+    player_code : player_code_generation(),
+    name : 'Player_name',
+    surname : 'Player_surname',
+    age : 30,
+    game_points_average : game_average(),
+    percentage_three_points : percentage_three_points_generation(),
+}
+
+
+
+
+
+
+
+
+
+// console log delle funzioni
+console.log( player_code_generation() );
+console.log( game_average() );
 console.log( percentage_three_points_generation() );
-
-
-// game statistics generation
-
+console.log( player_data_printing() );
 
 
 
